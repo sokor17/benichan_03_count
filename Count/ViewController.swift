8840bfeb42d1ff919bba07b68b741eaf18cab65a
YuKono <cosmos17128@gmail.com>
2021-05-12 15:51:35 +0900

    カウントアプリの完成

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -8,12 +8,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var number :Int = 0
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+   
+    @IBAction func plus() {
+        number = number + 1
+        label.text = String(number)
+        
+        if number >= 10 {
+            label.textColor = UIColor.red
+        } else if number <= -10{
+            label.textColor = UIColor.blue
+        } else{
+            label.textColor = UIColor.black
+        }
 
-
+    }
+    
+    @IBAction func minus(){
+        number = number - 1
+        label.text = String(number)
+        
+        
+        if number >= 10 {
+            label.textColor = UIColor.red
+        } else if number <= -10{
+            label.textColor = UIColor.blue
+        } else{
+            label.textColor = UIColor.black
+        }
+    }
+    
+    @IBAction func kakeru(){
+        number = number*number
+        label.text=String(number)
+        
+        if number >= 10 {
+            label.textColor = UIColor.red
+        } else if number <= -10{
+            label.textColor = UIColor.blue
+        } else{
+            label.textColor = UIColor.black
+        }
+    }
+    
+    @IBAction func waru(){
+        number = number/number
+        label.text=String(number)
+        
+        if number >= 10 {
+            label.textColor = UIColor.red
+        } else if number <= -10{
+            label.textColor = UIColor.blue
+        } else{
+            label.textColor = UIColor.black
+        }
+    }
+    
+    @IBAction func clear(){
+        number = 0
+        label.text=String(number)
+    }
+    
 }
-
